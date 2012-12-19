154f743188bd4683e9a660e9429b783dd5bb6db1
Florin Ciubotaru <florin.ciubotaru@ieee.org>
2012-12-19 18:00:26 +0200

    #1 Fork latest stable version of Zimbra - 8.0.1 IronMaiden - ThirdParty module stub(not including third party config and binaries)

---
+++ ThirdParty/beta_versions.sh
@@ -0,0 +1,14 @@
+cyrus_release=2.1.25
+cyrus_patchlevel=4z
+cyrus_version=${cyrus_release}.${cyrus_patchlevel}
+cyrus_src=cyrus-sasl-${cyrus_version}
+
+heimdal_version=1.5.2
+openssl_version=1.0.1c
+curl_version=7.25.0
+xml2_version=2.7.8
+
+openssl_lib_dir=/opt/zimbra/openssl-${openssl_version}/lib
+heimdal_lib_dir=/opt/zimbra/heimdal-${heimdal_version}/lib
+cyrus_lib_dir=/opt/zimbra/cyrus-sasl-${cyrus_version}/lib
+curl_lib_dir=/opt/zimbra/curl-${curl_version}/lib
