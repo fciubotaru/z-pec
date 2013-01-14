9df6e2141eb0e8248d1e21948f5191a3995ad38e
Cristian Sfercoci <cristian_sfercoci@yahoo.com>
2013-01-14 19:25:33 +0200

    #6 Create zimlet to filter mails based on a marker
    -add a  panel and search support

---
+++ Zimlet-PEC/src/com_pec_search/com_pec_search.js
@@ -0,0 +1,169 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Zimlets
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+com_pec_search_HandlerObject = function() {
+};
+com_pec_search_HandlerObject.prototype = new ZmZimletBase;
+com_pec_search_HandlerObject.prototype.constructor = com_pec_search_HandlerObject;
+
+/**
+ * Double clicked.
+ */
+com_pec_search_HandlerObject.prototype.doubleClicked = function() {
+	this.singleClicked();
+};
+
+/**
+ * Single clicked.
+ */
+com_pec_search_HandlerObject.prototype.singleClicked = function() {
+	this._displayDialog();
+};
+
+/**
+ * Displays the dialog.
+ * 
+ */
+com_pec_search_HandlerObject.prototype._displayDialog = function() {
+	if (this.pbDialog) { // if zimlet dialog already exists...
+		this.pbDialog.popup(); // simply popup the dialog
+		return;
+	}
+
+	var sDialogTitle = this.getMessage("simpledialog_dialog_title");
+	var sStatusMsg = this.getMessage("simpledialog_status_launch");
+
+	var search = appCtxt.getAppController();
+
+	// TODO replace current simple panel box with list DwtListView
+	this.pView = new DwtComposite(this.getShell()); // creates an empty div as a
+	// child of main shell div
+	this.pView.setSize("350", "250"); // set width and height
+	this.pView.getHtmlElement().style.overflow = "auto"; // adds scrollbar
+	this.pView.getHtmlElement().innerHTML = this._createDialogView(); // insert
+	// html
+	// to
+	// the
+	// dialogbox
+
+	// pass the title, view & buttons information to create dialog box
+	this.pbDialog = new ZmDialog({
+		title : sDialogTitle,
+		view : this.pView,
+		parent : this.getShell(),
+		standardButtons : [ DwtDialog.OK_BUTTON ]
+	});
+
+	this.pbDialog.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(this,
+			this._closeBtnListener));
+
+	this.pbDialog.popup(); // show the dialog
+
+	appCtxt.getAppController().setStatusMsg(sStatusMsg);
+	
+	//performing actual search
+	this.doInternalSearch();
+};
+
+/**
+ * Creates the dialog view.
+ * 
+ */
+com_pec_search_HandlerObject.prototype._createDialogView = function() {
+	var html = AjxTemplate.expand("com_pec_search.templates.table_temp#Main");
+	return html;
+};
+
+/**
+ * The "Close" button listener.
+ * 
+ */
+com_pec_search_HandlerObject.prototype._closeBtnListener = function() {
+
+	this.pbDialog.popdown(); // hide the dialog
+};
+
+/**
+ * Perform an internal search
+ * 
+ * @param _limit
+ * @param _offset
+ */
+com_pec_search_HandlerObject.prototype.doInternalSearch = function() {
+
+	var getHtml = appCtxt.get(ZmSetting.VIEW_AS_HTML);
+	var callbck = new AjxCallback(this, this._handleInternalSrcResponse);
+	var _types = new AjxVector();
+	// if is's necessary we will add more
+	_types.add(ZmId.ITEM_MSG);
+
+	// perform actual search
+	appCtxt.getSearchController().search({
+		query : "z-pec",
+		userText : true,
+		limit : 6500,
+		offset : 0,
+		types : _types,
+		noRender : true,
+		getHtml : getHtml,
+		callback : callbck
+	});
+};
+
+/**
+ * Handle internal response for what we've done
+ * 
+ * @author csfercoci
+ * @since 14.01.2012
+ * @param result
+ *            ajax result
+ */
+com_pec_search_HandlerObject.prototype._handleInternalSrcResponse = function(
+		result) {
+
+	var array = result.getResponse().getResults(ZmId.ITEM_MSG).getVector()
+			.getArray();
+	this._totalMsgArray = this._totalMsgArray.concat(array);
+	if (this._totalMsgArray.length == 0) {// no results
+		appCtxt.getAppController().setStatusMsg("No results");
+		return;
+	}
+
+	// done with searching, now process..
+	this._currentRequestNumber = 0;// reset
+	this._processResult(this._totalMsgArray, false);
+	this._totalMsgArray = [];
+
+	for ( var i = 0; i < array.length; i++) {
+		var eml = array[i];
+		this.senderArry.push(eml._addrs.FROM._array[0].address);
+		var tbl = document.getElementById('tbl_search_refiner'); // table
+		// reference
+		 var div = document.createElement('div'); // create DIV element
+	     var  txt = document.createTextNode(teml._addrs.FROM._array[0].address); // create
+																					// text
+																					// node
+	
+		row = tbl.insertRow(tbl.rows.length); // append table row		 
+	    div.appendChild(txt);                    // append text node to the DIV
+	    div.setAttribute('class', style);        // set DIV class attribute
+	    div.setAttribute('className', style);    // set DIV class attribute
+													// for IE (?!)
+	   // add content to the row
+	    row.insertCell(i).appendChild(div);
+
+	}
+
+};
